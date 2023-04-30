02abae3d7bd8b6b8c79adf2882217bbaafd1ca67
suryansh2206 <suryansh804.sg@gmail.com>
2023-04-30 22:35:30 +0530

    Home,Store,About added through route

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 // import logo from "./logo.svg";
 import React, { useState } from "react";
-import Header from './Components/Header/Header'
-import Cart from './Components/Cart/Cart'
+import Header from "./Components/Layout/Header";
+import Footer from "./Components/Layout/Footer";
+import Cart from "./Components/Cart/Cart";
 import Button from "./Components/UI/Button";
 import "./App.css";
 import Products from "./Components/Products/Products";
-import ContextProvider from './Store/ContextProvider'
+import ContextProvider from "./Store/ContextProvider";
+import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
 // import { Button } from "bootstrap";
 
 const App = () => {
-
-  const[isCartOpen, setCartOpen] = useState(false)
+  const [isCartOpen, setCartOpen] = useState(false);
   const closeHandler = () => {
-    setCartOpen(false)
-  }
+    setCartOpen(false);
+  };
   const openHandler = () => {
-    setCartOpen(true)
-  }
+    setCartOpen(true);
+  };
 
   return (
     <ContextProvider>
       {isCartOpen && <Cart onClose={closeHandler} />}
       <Header onOpen={openHandler} />
-      <main>
-        <Products />
-      </main>
-      <Button onClick={openHandler}>Open Cart</Button>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <main>
+                <Products />
+              </main>
+              <Button onClick={openHandler}>Open Cart</Button>
+            </>
+          }
+        />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
     </ContextProvider>
   );
-  
 };
 
 export default App;
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -2,6 +2,7 @@ import React, { Fragment, useContext } from "react";
 import Button from "../UI/Button";
 import "./Header.css";
 import CartContext from "../../Store/cart-context";
+import { NavLink } from "react-bootstrap";
 
 const Header = (props) => {
   const ctxobj = useContext(CartContext);
@@ -14,9 +15,18 @@ const Header = (props) => {
     <Fragment>
       <div className="container">
         <ul className="header-items">
-          <li>Home</li>
+          {/* <li>Home</li>
           <li>Store</li>
-          <li>About</li>
+          <li>About</li> */}
+          <NavLink className="link" to="/home">
+            <div>Home</div>
+          </NavLink>
+          <NavLink className="link" to="/store">
+            <div>Store</div>
+          </NavLink>
+          <NavLink className="link" to="/about">
+            <div>About</div>
+          </NavLink>
           <li>
             <Button onClick={props.onOpen}>Cart {totalQuantity}</Button>
           </li>
